8f77d91aaa0ec7bffa0edde569063271a979ca24
Lazy Cat <yagi.st123@gmail.com>
2025-09-29 20:26:20 +0700

    Upload some old ideas

---
+++ include/memory/gc_disable_guard.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "memory/memory_manager.h"
+
+class GCDisableGuard {
+private:
+    MemoryManager* heap_;
+public:
+    explicit GCDisableGuard(MemoryManager* heap) noexcept : heap_(heap) { 
+        if (heap_) heap_->disable_gc();
+    }
+    GCDisableGuard(const GCDisableGuard&) = delete;
+    GCDisableGuard& operator=(const GCDisableGuard&) = delete;
+    GCDisableGuard(GCDisableGuard&&) = delete;
+    GCDisableGuard& operator=(GCDisableGuard&&) = delete;
+    ~GCDisableGuard() noexcept { 
+        if (heap_) heap_->enable_gc(); 
+    }
+};
